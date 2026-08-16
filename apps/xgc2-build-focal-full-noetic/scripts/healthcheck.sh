c4c8baa32404bb1ef25569289e0765415b72a3e4
Lxk_buaa <867768510@qq.com>
2026-08-17 04:11:06 +0800

    Add JSON headers to Noetic full image

--- apps/xgc2-build-focal-full-noetic/scripts/healthcheck.sh
+++ apps/xgc2-build-focal-full-noetic/scripts/healthcheck.sh
@@ -12,7 +12,7 @@ command -v gazebo >/dev/null
 dpkg-query -W ros-noetic-pcl-ros ros-noetic-pcl-conversions \
   ros-noetic-eigen-conversions ros-noetic-tf libgflags-dev \
   libgoogle-glog-dev libtbb-dev libyaml-cpp-dev libpcl-dev \
-  libffmpeg-nvenc-dev >/dev/null
+  libffmpeg-nvenc-dev nlohmann-json3-dev >/dev/null
 if dpkg-query -W -f='${Package}\n' | grep -E '^(lib)?xgc2-|ros-[a-z]+-xgc2-'; then
   echo "XGC2 packages leaked into build image" >&2
   exit 1
